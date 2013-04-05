ba3d1dd56170368b379c5a169b485434ce63d1ac
Gant <GantMan@gmail.com>
2013-04-04 20:51:38 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Motionmeetup::Application.config.secret_token = '0881d3860124e1da513c5c90acb37798197113c007c9a24a6bbf6eefb4300ba867c18537a09d61732728bb3f91a4dad4dc74b4412b3a30c5214cc16ba4883abb'
