0568cce254a573994bae2150251a51a503b0f326
Matt Garrison <mattsgarrison@iconoclastlabs.com>
2013-05-18 12:54:53 -0500

    updating user model migrations, bumped ruby version

--- db/migrate/20130405015637_create_users.rb
+++ db/migrate/20130405015637_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
+      t.string :name, :default => ""
+      t.string :email, :null => false
       t.string :provider
       t.string :uid
 
