1cc87c6102723a1e8b289167af9f8b0c796091dc
Matt Garrison <mattsgarrison@iconoclastlabs.com>
2013-05-29 15:10:08 -0500

    annotate

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -2,18 +2,18 @@
 #
 # Table name: archives
 #
-#  id                  :integer          not null, primary key
-#  name                :string(255)
-#  title               :string(255)
-#  body                :text
-#  publish_at          :datetime
-#  created_at          :datetime
-#  updated_at          :datetime
-#  slug                :string(255)
-#  assets_file_name    :string(255)
-#  assets_content_type :string(255)
-#  assets_file_size    :integer
-#  assets_updated_at   :datetime
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  title              :string(255)
+#  body               :text
+#  publish_at         :datetime
+#  created_at         :datetime
+#  updated_at         :datetime
+#  slug               :string(255)
+#  asset_file_name    :string(255)
+#  asset_content_type :string(255)
+#  asset_file_size    :integer
+#  asset_updated_at   :datetime
 #
 
 class Archive < ActiveRecord::Base
