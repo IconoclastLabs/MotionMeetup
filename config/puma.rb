c84202b4dec0ee43604fb21404f79f0eba5d0cd4
Matt Garrison <mattsgarrison@iconoclastlabs.com>
2013-07-05 09:07:43 -0500

    twiddling puma config

--- config/puma.rb
+++ config/puma.rb
@@ -128,11 +128,11 @@ workers 3
 # activate_control_app 'unix:///var/run/pumactl.sock', { auth_token: '12345' }
 # activate_control_app 'unix:///var/run/pumactl.sock', { no_token: true }
 
-require "active_record"
-cwd = File.dirname(__FILE__)+"/.."
-ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[ENV["RACK_ENV"]])
-ActiveRecord::Base.verify_active_connections!
+# require "active_record"
+# cwd = File.dirname(__FILE__)+"/.."
+# ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+# ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[ENV["RACK_ENV"]])
+# ActiveRecord::Base.verify_active_connections!
 
 # if you use NewRelic, set your NEWRELIC_DISPATCHER environment variable on heroku, per
 # https://github.com/puma/puma/issues/128 - may not be needed for future releases of Puma
