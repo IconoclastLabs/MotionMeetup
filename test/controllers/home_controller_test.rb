9e8b51e0178ba299088e8aab06e0eca45d36b817
Matt Garrison <matt@iconoclastlabs.com>
2013-08-13 11:13:42 -0500

    adding a simple test for the signup action

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,4 +6,10 @@ class HomeControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+  
+  def test_signup
+    get :signup, message: {name:"test", email: "example@example.com", captcha: "63"}
+    assert_response :success
+
+  end
 end
